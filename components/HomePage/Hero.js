916a13bb414029ba840b8a48a8fba0afff73b11f
backendsamurai <katsuronosuri@hotmail.com>
2022-07-15 03:17:36 +0500

    Fix On Mobile 2

--- components/HomePage/Hero.js
+++ components/HomePage/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
       <section className="px-4 lg:px-0 container text-white">
         <section className="min-h-screen flex flex-col items-center justify-center gap-12 lg:items-start">
           <section className="text-center lg:text-left">
-            <h2 className="mb-1 text-sm font-semibold uppercase tracking-widest md:mb-5 md:text-xl">
+            <h2 className="mb-1 text-xs font-semibold uppercase tracking-widest md:mb-5 md:text-xl">
               Welcome to <Logo />
             </h2>
             <h1 className="max-w-3xl font-bold text-2xl leading-normal md:text-4xl lg:text-5xl xl:text-6xl md:leading-normal lg:leading-snug xl:leading-tight">
